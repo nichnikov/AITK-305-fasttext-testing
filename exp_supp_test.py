9bfcb1516d0fc9ee1888a71f4dcc82530e6a8416
Ничников Алексей Сергеевич <nichnikov@action-media.ru>
2023-03-01 22:21:46 +0300

    working test service

--- exp_supp_test.py
+++ exp_supp_test.py
@@ -3,14 +3,17 @@ import pandas as pd
 from src.start import classifier
 
 test_qrs_df = pd.read_csv(os.path.join("data", "queries_for_test.csv"), sep="\t")
-test_qrs_df = test_qrs_df[:10]
+test_qrs_df = test_qrs_df
+
 
 results = []
+k = 1
 for chat_id, query in zip(test_qrs_df["chat_id"], test_qrs_df["text"]):
     result = classifier.searching(query)
     result_ = (chat_id, query) + result
-    print(result_)
     results.append(result_)
+    print(k, "/", len(test_qrs_df))
+
 
 print(results)
 results_df = pd.DataFrame(results, columns=["ChatId", "Query", "FastAnswID", "Etalon", "LemEtalon", "FTScore"])
